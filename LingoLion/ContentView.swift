9d0b930a21256ee31bb5835da4c25ea8fb2ba495
Marcelo Monsalve <mm.hakanson@gmail.com>
2023-03-04 11:44:34 -0800

    Working implementation of chatgpt3 turbo with swift!

--- LingoLion/ContentView.swift
+++ LingoLion/ContentView.swift
@@ -6,16 +6,69 @@
 //
 
 import SwiftUI
+import OpenAI
+
+
+
+class ViewModel: ObservableObject {
+    let openAI: OpenAI
+    @Published var messages: [String]
+    
+    init() {
+        openAI = OpenAI(apiToken: "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")
+        messages = [String]()
+    }
+    
+    func send(question: String,
+              completion: @escaping (String) -> Void) async {
+        let query = OpenAI.ChatQuery(model: .gpt3_5Turbo, messages: [.init(role: "user", content: question)])
+        do {
+            let result = try await self.openAI.chats(query: query)
+            completion((result.choices.first?.message.content ?? "").trimmingCharacters(in: .whitespacesAndNewlines))
+        } catch {
+            print("Error code 1738")
+        }
+    }
+    
+    func addMessage(text: String) {
+        self.messages.append(text)
+    }
+}
+
 
 struct ContentView: View {
+    @ObservedObject var viewModel = ViewModel()
+    @State var text = ""
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            Text("Lingo Lion")
+                .font(.title)
+                            .fontWeight(.bold)
+            List(viewModel.messages, id: \.self) { string in
+                Text(string)
+            }
+            Spacer()
+            HStack {
+                TextField("Type here...", text: $text)
+                Button("Send") {
+                    ButtonSend()
+                }
+            }
+            .padding()
         }
-        .padding()
+    }
+    
+    func ButtonSend() {
+        viewModel.addMessage(text: "Me: \(text)")
+        Task{
+            await viewModel.send(question: text) { response in
+                DispatchQueue.main.async {
+                    viewModel.addMessage(text: "ChatGPT: \(response)")
+                }
+                
+            }
+        }
     }
 }
 
