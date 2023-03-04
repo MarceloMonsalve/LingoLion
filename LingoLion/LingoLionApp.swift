8e0b10859aa98d261c96243d29668d891b48b5f9
Marcelo Monsalve <mm.hakanson@gmail.com>
2023-03-04 09:09:40 -0800

    Initial Commit

---
+++ LingoLion/LingoLionApp.swift
@@ -0,0 +1,17 @@
+//
+//  LingoLionApp.swift
+//  LingoLion
+//
+//  Created by Marcelo Monsalve on 3/4/23.
+//
+
+import SwiftUI
+
+@main
+struct LingoLionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
