7bb4a6a2cd8f2d672a38323491603b0ddeb73c06
Marcelo Monsalve <mm.hakanson@gmail.com>
2023-03-06 10:15:06 -0800

    Now includes functionality for chat history and initial prompt.

--- LingoLion/ContentView.swift
+++ LingoLion/ContentView.swift
@@ -8,34 +8,80 @@
 import SwiftUI
 import OpenAI
 
+let systemMessage = "You are Lingo Lion a language learning helper that has practice conversations with people learning a new language on various topics."
+let apikey = "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"
 
-
+// ViewModel for the main chat with the AI. Keeps track of two chats: one is the one that gets shown to the user and one is the one sent to the api. The api chat must be kept under 2500 tokens (not yet implemented.
 class ViewModel: ObservableObject {
     let openAI: OpenAI
-    @Published var messages: [String]
+    var gptChat: [OpenAI.Chat]
+    @Published var chat: [String]
     
+    // Initializes the ViewModel and sends the api an initial "Hello"
     init() {
-        openAI = OpenAI(apiToken: "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")
-        messages = [String]()
+        openAI = OpenAI(apiToken: apikey)
+        chat = [String]()
+        gptChat = [.init(role: "system", content: systemMessage), .init(role: "user", content: "Hello"),]
+        Task {
+            await self.apiCall() { response in
+                self.addMessage(role: "assistant",text: response)
+            }
+        }
     }
     
-    func send(question: String,
-              completion: @escaping (String) -> Void) async {
-        let query = OpenAI.ChatQuery(model: .gpt3_5Turbo, messages: [.init(role: "user", content: question)])
+    // Helper function to change the role to how it will be displayed
+    func role(role: String) -> String {
+        switch role {
+        case "assistant": return "Lingo Lion"
+        case "user": return "You"
+        default: return role
+        }
+    }
+    
+    // Sends gptChat array to the api and if theres an error then it calls it recursively until it works. Sends a string with the chatgpt's response to the completion.
+    func apiCall(completion: @escaping (String) -> Void) async {
+        let query = OpenAI.ChatQuery(model: .gpt3_5Turbo, messages: gptChat)
         do {
             let result = try await self.openAI.chats(query: query)
             completion((result.choices.first?.message.content ?? "").trimmingCharacters(in: .whitespacesAndNewlines))
         } catch {
             print("Error code 1738")
+            await self.apiCall { string in
+                completion(string)
+            }
+            
         }
     }
     
-    func addMessage(text: String) {
-        self.messages.append(text)
+    // Called when the user presses send. Calls apiCall with new response from user and updates chat and gptChat through addMessage accordingly.
+    func send(text: String) {
+        self.addMessage(role: "user",text: text)
+        Task {
+            await self.apiCall() { response in
+                self.addMessage(role: "assistant", text: response)
+            }
+        }
+    }
+    
+    // Appends given message to chat and gptChat arrays. Deletes duplicate entries from gptChat and in future will be responsible for keeping gptChat under 2500 tokens.
+    func addMessage(role: String, text: String) {
+        var i = 0
+        for message in self.gptChat {
+            if message.content == text && message.role == role {
+                self.gptChat.remove(at: i)
+                break
+            }
+            i += 1
+        }
+        self.gptChat.append(.init(role: role, content: text))
+        DispatchQueue.main.async {
+            self.chat.append("\(self.role(role: role)): \(text)")
+        }
+        
     }
 }
 
-
+// The View for the main page of the app
 struct ContentView: View {
     @ObservedObject var viewModel = ViewModel()
     @State var text = ""
@@ -44,30 +90,20 @@ struct ContentView: View {
         VStack {
             Text("Lingo Lion")
                 .font(.title)
-                            .fontWeight(.bold)
-            List(viewModel.messages, id: \.self) { string in
-                Text(string)
+                .fontWeight(.bold)
+            
+            List(viewModel.chat, id: \.self) { message in
+                Text(message)
             }
             Spacer()
             HStack {
                 TextField("Type here...", text: $text)
                 Button("Send") {
-                    ButtonSend()
+                    viewModel.send(text: text)
+                    text = ""
                 }
             }
             .padding()
-        }
-    }
-    
-    func ButtonSend() {
-        viewModel.addMessage(text: "Me: \(text)")
-        Task{
-            await viewModel.send(question: text) { response in
-                DispatchQueue.main.async {
-                    viewModel.addMessage(text: "ChatGPT: \(response)")
-                }
-                
-            }
         }
     }
 }
